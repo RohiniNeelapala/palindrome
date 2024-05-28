015b8a8d8458d9cbae8438e04f017ce0e5fdb785
chandrakanth0609 <chandrakanth.mulupuri@gmail.com>
2024-05-28 17:21:40 +0530

    modified

--- palindrome.java
+++ palindrome.java
@@ -6,7 +6,7 @@ class palindrome
            while(num>0)
            {
               int rem=num%10;
-              reverse=reverse*10+rem;
+              reverse=reverse*10+rem;//this is palindrome
               num=num/10;
            }
            if(reverse==temp)
